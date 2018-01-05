7b5960eac14e22dc2a56f3d68dc47dad7d97a89e
Unknown <contact@xbird.me>
2018-01-05 19:59:09 +0100

    update xbirdconfigs
    change file url + add esl config file

--- xbirdconfigs.sh
+++ xbirdconfigs.sh
@@ -4,5 +4,6 @@
 source config.sh
 #Execution
 
-curl -o $SERVERDIR/cfg/xbirdme-csgo-cfgservermatch.cfg https://github.com/Xbirdfr/CSGO-CFGSERVERMATCH/raw/master/xbirdme-csgo-cfgservermatch.cfg
-curl -o $SERVERDIR/cfg/gamemode_competitive_server.cfg https://github.com/Xbirdfr/CSGO-CFGSERVERMATCH/raw/master/https://github.com/Xbirdfr/CSGO-CFGSERVERMATCH/raw/master/gamemode_competitive_server.cfg
+curl -o $SERVERDIR/cfg/xbirdme-csgo-cfgservermatch.cfg https://raw.githubusercontent.com/Xbirdfr/CSGO-CFGSERVERMATCH/master/xbirdme-csgo-cfgservermatch.cfg
+curl -o $SERVERDIR/cfg/gamemode_competitive_server.cfg https://raw.githubusercontent.com/Xbirdfr/CSGO-CFGSERVERMATCH/master/gamemode_competitive_server.cfg
+curl -o $SERVERDIR/cfg/esl5on5-fixedtimer.cfg https://raw.githubusercontent.com/Xbirdfr/CSGO-CFGSERVERMATCH/master/esl5on5-fixedtimer.cfg
